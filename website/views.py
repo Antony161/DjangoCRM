555833a23b28c88f7805a2875fe412f8a8d0325c
Antonyeyyo <antonyeyyo@gmail.com>
2025-01-17 06:52:15 +0000

    final_commit

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm,AddrecordForm
 from .models import Record
 
 # Create your views here.
@@ -69,6 +69,46 @@ def customer_record(request,pk):
     else:
         messages.success(request,"you have to be logged in for viewing that record")
         return redirect('home')
+
+def delete_record(request,pk):
+    if request.user.is_authenticated:
+        delete_it=Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request,"record has been deleted sucessfully")
+        return redirect('home')
+    else:
+        messages.success(request,"you must be logged in for deleting record")
+        return redirect('home')
+
+
+
+def add_record(request):
+    form=AddrecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method=="POST":
+            if form.is_valid():
+                add_record=form.save()
+                messages.success(request,"Record added")
+                return redirect('home')
+        return render(request,'add_record.html',{'form':form})
+    else:
+        messages.success(request,"you must be logged in to add record")
+        return redirect('home')
+
+
+def update_record(request,pk):
+    if request.user.is_authenticated:
+        current_record=Record.objects.get(id=pk)
+        form=AddrecordForm(request.POST or None,instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Records has been updated")
+            return redirect('home')
+        return render(request,'update_record.html',{'form':form})
+    else:
+        messages.success(request,"you must be logged in for updating record")
+        return redirect('home')
+
         
 
 
